349408cf60b8e6f5b6d17ab175d37065f8e1831d
mind106 <minhnguyen300520@gmail.com>
2021-10-23 22:23:57 +0700

    Hello

---
+++ app/src/main/java/com/rajendra/onlinedailygroceries/AllCategory.java
@@ -0,0 +1,142 @@
+package com.rajendra.onlinedailygroceries;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.rajendra.onlinedailygroceries.adapter.AllCategoryAdapter;
+import com.rajendra.onlinedailygroceries.model.AllCategoryModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllCategory extends AppCompatActivity {
+
+    RecyclerView AllCategoryRecycler;
+    AllCategoryAdapter allCategoryAdapter;
+    List<AllCategoryModel> allCategoryModelList;
+
+    ImageView Back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_category);
+
+        AllCategoryRecycler = findViewById(R.id.all_category);
+        Back = findViewById(R.id.Back);
+
+        Back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent Back = new Intent(AllCategory.this, MainActivity.class);
+                startActivity(Back);
+                finish();
+            }
+        });
+
+
+        // adding data to model
+        allCategoryModelList = new ArrayList<>();
+        allCategoryModelList.add(new AllCategoryModel(1, R.drawable.ic_fruits));
+        allCategoryModelList.add(new AllCategoryModel(2, R.drawable.ic_veggies));
+        allCategoryModelList.add(new AllCategoryModel(3, R.drawable.ic_meat));
+        allCategoryModelList.add(new AllCategoryModel(4, R.drawable.ic_fish));
+        allCategoryModelList.add(new AllCategoryModel(5, R.drawable.ic_spices));
+        allCategoryModelList.add(new AllCategoryModel(6, R.drawable.ic_egg));
+        allCategoryModelList.add(new AllCategoryModel(7, R.drawable.ic_drink));
+        allCategoryModelList.add(new AllCategoryModel(8, R.drawable.ic_cookies));
+        allCategoryModelList.add(new AllCategoryModel(1, R.drawable.ic_fruits));
+        allCategoryModelList.add(new AllCategoryModel(2, R.drawable.ic_veggies));
+        allCategoryModelList.add(new AllCategoryModel(3, R.drawable.ic_meat));
+        allCategoryModelList.add(new AllCategoryModel(4, R.drawable.ic_fish));
+        allCategoryModelList.add(new AllCategoryModel(5, R.drawable.ic_spices));
+        allCategoryModelList.add(new AllCategoryModel(6, R.drawable.ic_egg));
+        allCategoryModelList.add(new AllCategoryModel(7, R.drawable.ic_drink));
+        allCategoryModelList.add(new AllCategoryModel(8, R.drawable.ic_cookies));
+        allCategoryModelList.add(new AllCategoryModel(1, R.drawable.ic_fruits));
+        allCategoryModelList.add(new AllCategoryModel(2, R.drawable.ic_veggies));
+        allCategoryModelList.add(new AllCategoryModel(3, R.drawable.ic_meat));
+        allCategoryModelList.add(new AllCategoryModel(4, R.drawable.ic_fish));
+        allCategoryModelList.add(new AllCategoryModel(5, R.drawable.ic_spices));
+        allCategoryModelList.add(new AllCategoryModel(6, R.drawable.ic_egg));
+        allCategoryModelList.add(new AllCategoryModel(7, R.drawable.ic_drink));
+        allCategoryModelList.add(new AllCategoryModel(8, R.drawable.ic_cookies));
+        allCategoryModelList.add(new AllCategoryModel(1, R.drawable.ic_fruits));
+        allCategoryModelList.add(new AllCategoryModel(2, R.drawable.ic_veggies));
+        allCategoryModelList.add(new AllCategoryModel(3, R.drawable.ic_meat));
+        allCategoryModelList.add(new AllCategoryModel(4, R.drawable.ic_fish));
+        allCategoryModelList.add(new AllCategoryModel(5, R.drawable.ic_spices));
+        allCategoryModelList.add(new AllCategoryModel(6, R.drawable.ic_egg));
+        allCategoryModelList.add(new AllCategoryModel(7, R.drawable.ic_drink));
+        allCategoryModelList.add(new AllCategoryModel(8, R.drawable.ic_cookies));
+
+
+
+        setCategoryRecycler(allCategoryModelList);
+
+    }
+
+    private void setCategoryRecycler(List<AllCategoryModel> allcategoryModelList) {
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 3);
+        AllCategoryRecycler.setLayoutManager(layoutManager);
+        AllCategoryRecycler.addItemDecoration(new GridSpacingItemDecoration(3, dpToPx(32), true));
+        AllCategoryRecycler.setItemAnimator(new DefaultItemAnimator());
+        allCategoryAdapter = new AllCategoryAdapter(this, allCategoryModelList);
+        AllCategoryRecycler.setAdapter(allCategoryAdapter);
+    }
+    // now we need some item decoration class for manage spacing
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+}
---
+++ app/src/main/java/com/rajendra/onlinedailygroceries/MainActivity.java
@@ -0,0 +1,113 @@
+package com.rajendra.onlinedailygroceries;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.rajendra.onlinedailygroceries.adapter.CategoryAdapter;
+import com.rajendra.onlinedailygroceries.adapter.DiscountedProductAdapter;
+import com.rajendra.onlinedailygroceries.adapter.RecentlyViewedAdapter;
+import com.rajendra.onlinedailygroceries.model.Category;
+import com.rajendra.onlinedailygroceries.model.DiscountedProducts;
+import com.rajendra.onlinedailygroceries.model.RecentlyViewed;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView discountRecyclerView, categoryRecyclerView, recentlyViewedRecycler;
+    DiscountedProductAdapter discountedProductAdapter;
+    List<DiscountedProducts> discountedProductsList;
+
+    CategoryAdapter categoryAdapter;
+    List<Category> categoryList;
+
+    RecentlyViewedAdapter recentlyViewedAdapter;
+    List<RecentlyViewed> recentlyViewedList;
+
+    ImageView allCategory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        discountRecyclerView = findViewById(R.id.discountedRrcycler);
+        categoryRecyclerView = findViewById(R.id.categoryRecycler);
+        allCategory = findViewById(R.id.allCategoryImage);
+        recentlyViewedRecycler = findViewById(R.id.recently_item);
+
+        allCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AllCategory.class);
+                startActivity(i);
+            }
+        });
+
+
+        // adding data to model
+
+        discountedProductsList = new ArrayList<>();
+        discountedProductsList.add(new DiscountedProducts(1, R.drawable.discountberry));
+        discountedProductsList.add(new DiscountedProducts(2, R.drawable.discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(3, R.drawable.discountmeat));
+        discountedProductsList.add(new DiscountedProducts(1, R.drawable.discountberry));
+        discountedProductsList.add(new DiscountedProducts(2, R.drawable.discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(3, R.drawable.discountmeat));
+        discountedProductsList.add(new DiscountedProducts(1, R.drawable.discountberry));
+
+        // adding data to model
+        categoryList = new ArrayList<>();
+        categoryList.add(new Category(3, R.drawable.ic_home_meats));
+        categoryList.add(new Category(4, R.drawable.ic_home_veggies));
+        categoryList.add(new Category(2, R.drawable.ic_home_fruits));
+        categoryList.add(new Category(1, R.drawable.ic_home_fish));
+        categoryList.add(new Category(2, R.drawable.ic_home_fruits));
+        categoryList.add(new Category(3, R.drawable.ic_home_meats));
+        categoryList.add(new Category(4, R.drawable.ic_home_veggies));
+        categoryList.add(new Category(2, R.drawable.ic_home_fruits));
+
+        // adding data to model
+        recentlyViewedList = new ArrayList<>();
+        recentlyViewedList.add(new RecentlyViewed("Watermelon", "Watermelon has high water content and also provides some fiber.", "$ 80", "1", "KG", R.drawable.card4));
+        recentlyViewedList.add(new RecentlyViewed("Papaya", "Watermelon has high water content and also provides some fiber.", "$ 80", "1", "KG", R.drawable.card3));
+        recentlyViewedList.add(new RecentlyViewed("Strawberry", "Watermelon has high water content and also provides some fiber.", "$ 80", "1", "KG", R.drawable.card2));
+        recentlyViewedList.add(new RecentlyViewed("Kiwi", "Watermelon has high water content and also provides some fiber.", "$ 80", "1", "KG", R.drawable.card1));
+        recentlyViewedList.add(new RecentlyViewed("Watermelon", "Watermelon has high water content and also provides some fiber.", "$ 80", "1", "KG", R.drawable.card4));
+
+
+
+        setDiscountedRecycler(discountedProductsList);
+        setCategoryRecycler((categoryList));
+        setRecentlyViewedRecycler(recentlyViewedList);
+
+    }
+
+    private void setDiscountedRecycler(List<DiscountedProducts> dataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        discountRecyclerView.setLayoutManager(layoutManager);
+        discountedProductAdapter = new DiscountedProductAdapter(this, dataList);
+        discountRecyclerView.setAdapter(discountedProductAdapter);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryDataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        categoryRecyclerView.setLayoutManager(layoutManager);
+        categoryAdapter = new CategoryAdapter(this,categoryDataList);
+        categoryRecyclerView.setAdapter(categoryAdapter);
+    }
+
+    private void setRecentlyViewedRecycler(List<RecentlyViewed> recentlyViewedDataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recentlyViewedRecycler.setLayoutManager(layoutManager);
+        recentlyViewedAdapter = new RecentlyViewedAdapter(this,recentlyViewedDataList);
+        recentlyViewedRecycler.setAdapter(recentlyViewedAdapter);
+    }
+}
